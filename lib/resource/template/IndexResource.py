a57e800da9b7130469b68413b9b316302449fcce
kuro2a <redagma2+dev@gmail.com>
2020-07-11 12:14:51 +0900

    [modified] Fix top page title.

--- lib/resource/template/IndexResource.py
+++ lib/resource/template/IndexResource.py
@@ -11,4 +11,4 @@ from lib.resource import BaseHtmlTemplateResource
 class IndexResource(BaseHtmlTemplateResource):
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
-        resp.body = self.get_content('index.html.j2', {'title': 'Login'})
+        resp.body = self.get_content('index.html.j2', {'title': 'Top'})
